e6dc694e019d47d4f2158f117e5be8020a7f3df0
JC <jc.alonsome@gmail.com>
2023-03-22 17:12:18 +0100

    oki

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,27 +1,28 @@
+import RatingCard from '../ratingCard/RatingCard'
 import style from './header.module.scss'
-
+import { v4 } from 'uuid'
+const cards = [
+    {id: v4(),
+    text: 'Rated 5 Stars in Reviews'},
+    {id: v4(),
+    text: 'Rated 5 Stars in Report Guru'},
+    {id: v4(),
+    text: 'Rated 5 Stars in BestTech'}
+]
 const Header=()=>{
     return(
         <header className={style.header}>
             <div>
+                
                 <h1 className={style['header-title']}>10,000+ of our users love our products.</h1>
-                <p>We only provide great products combined with excellent customer service.
+                <p className={style['header-text']}>We only provide great products combined with excellent customer service.
                      See what our satisfied customers are saying about our services.</p>
             </div>
-            <div>
-            <div>
-                <div>stars</div>
-                <p>text</p>
+            <div className={style['header-cards']}>
+            {(cards).map((e) => 
+                <RatingCard text={e.text} key={e.id}/>)}
+            
             </div>
-            <div>
-                <div>stars</div>
-                <p>text</p>
-            </div>
-            <div>
-                <div>stars</div>
-                <p>text</p>
-            </div>
-        </div>
         </header>
     )
 }
---
+++ src/components/ratingCard/RatingCard.jsx
@@ -0,0 +1,18 @@
+import { v4 } from 'uuid'
+import style from './ratingCard.module.scss'
+const stars = 5
+
+const RatingCard=({text})=>{
+    return(
+        <div className={style['rating-card']}>
+                <div className={style['rating-card-stars-container']}>
+                {[...Array(stars)].map((e) => 
+                <img key={v4()} src="/images/icon-star.svg" alt="" />)}
+                </div>
+               
+                <p>{text}</p>
+            </div>
+    )
+}
+
+export default RatingCard
